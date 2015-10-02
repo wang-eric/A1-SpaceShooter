89518ecc4843571ab7b6e7045635e2f3346837fa
wang-eric <ericwang1988@hotmail.com>
2015-10-02 11:08:32 -0400

    Player Model Update
    Changed the plane to a car. Changed background music, and shot sound.

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -30,7 +30,6 @@ public class DestroyByContact : MonoBehaviour {
 		if (other.tag != "Player"){
 			gameController.AddScore (scoreValue);
 		}
-
 		Destroy(other.gameObject);
 		Destroy(gameObject);
 	}
